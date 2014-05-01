49bee70359608910b87d4988fb1a4a47f52ce60f
Tyler Weaver <tylerjw@gmail.com>
2014-05-01 12:07:52 -0600

    first commit

---
+++ src/com/bignerdranch/android/criminalintent/CrimeLab.java
@@ -0,0 +1,40 @@
+package com.bignerdranch.android.criminalintent;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+import android.content.Context;
+
+public class CrimeLab {
+	private static CrimeLab sCrimeLab;
+	private Context mAppContext;
+	private ArrayList<Crime> mCrimes;
+	
+	private CrimeLab(Context appContext) {
+		mAppContext = appContext;
+		mCrimes = new ArrayList<Crime>();
+	}
+	
+	public void addCrime(Crime c) {
+		mCrimes.add(c);
+	}
+	
+	public static CrimeLab get(Context c) {
+		if(sCrimeLab == null) {
+			sCrimeLab = new CrimeLab(c.getApplicationContext());
+		}
+		return sCrimeLab;
+	}
+	
+	public ArrayList<Crime> getCrimes() {
+		return mCrimes;
+	}
+
+	public Crime getCrime(UUID id) {
+		for(Crime c : mCrimes) {
+			if(c.getId().equals(id))
+				return c;
+		}
+		return null;
+	}
+}
